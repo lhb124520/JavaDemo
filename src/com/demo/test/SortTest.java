03f8fcee74dfd88f9d2f00957144295fc5e9eef4
liu.hb <1282072571@qq.com>
2018-09-14 15:52:11 +0800

    新增排序

--- src/com/demo/test/SortTest.java
+++ src/com/demo/test/SortTest.java
@@ -2,9 +2,102 @@ package com.demo.test;
 
 public class SortTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private int[] array;
+	private int length;
 
+	/**
+	 * 构造函数
+	 * 
+	 * @param array
+	 */
+	public SortTest(int[] array) {
+		this.array = array;
+		this.length = array.length;
+	}
+
+	/**
+	 * 打印数组中数据
+	 */
+	public void display(int[] array) {
+		for (int i : array) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+
+	/**
+	 * 冒泡排序
+	 */
+	public void bobSort() {
+		for (int i = 0; i < length - 1; i++) {// 排序轮数
+			for (int j = 0; j < length - 1; j++) {// 比较次数
+				if (array[j] > array[j + 1]) {
+					int temp = array[j + 1];
+					array[j + 1] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+	}
+
+	/**
+	 * 快速排序
+	 */
+	public static void quickSort(int array[], int low, int high) {		
+		// 1,找到递归算法的出口
+		if (low > high) {
+			return;
+		}
+		// 2, 存
+		int i = low;
+		int j = high;
+		// 3,key
+		int key = array[low];
+		// 4，完成一趟排序
+		while (i < j) {
+			// 4.1 ，从右往左找到第一个小于key的数
+			while (i < j && array[j] > key) {
+				j--;
+			}
+			// 4.2 从左往右找到第一个大于key的数
+			while (i < j && array[i] <= key) {
+				i++;
+			}
+			// 4.3 交换
+			if (i < j) {
+				int p = array[i];
+				array[i] = array[j];
+				array[j] = p;
+			}
+		}
+		// 4.4，调整key的位置
+		int p = array[i];
+		array[i] = array[low];
+		array[low] = p;
+		// 5, 对key左边的数快排
+		quickSort(array, low, i - 1);
+		// 6, 对key右边的数快排
+		quickSort(array, i + 1, high);
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 77, 29, 28, 36, 33, 25, 10 };
+		SortTest bobSort = new SortTest(array);
+		System.out.println("冒泡排序前的数据为：");
+		bobSort.display(array);
+		
+		bobSort.bobSort();		
+		
+		System.out.println("冒泡排序后的数据为：");
+		bobSort.display(array);
+		
+		int[] array1 = { 77, 29, 28, 36, 33, 25, 10 };
+		SortTest bobSort1 = new SortTest(array1);
+		System.out.println("快速排序前的数据为：");
+		bobSort1.display(array1);
+		quickSort(array1,0,array1.length-1);
+		System.out.println("快速排序后的数据为：");
+		bobSort.display(array1);
 	}
 
 }
--- src/com/demo/test/Test.java
+++ src/com/demo/test/Test.java
@@ -1,16 +1,6 @@
 package com.demo.test;
-
-import java.io.IOException;
-
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class Test {
 
@@ -21,7 +11,7 @@ public class Test {
 //        
 //		int []i=new int[5];
 //		System.out.println(i[4]);
-		clientTest("https://www.baidu.com/","");
+
 		Integer a = 1;
         Integer b = 2;
         Integer c = 3;
@@ -36,38 +26,16 @@ public class Test {
         System.out.println(g == (a + b));
         System.out.println(g.equals(a + b));
 		System.out.println(g.equals(a + b));
-		
+		 HashSet<String> hashSet=new HashSet<String>();
+	        hashSet.add("1");
+	        hashSet.add("2");
+	        hashSet.add("1");
+	        Iterator<String> iterator = hashSet.iterator();
+	        while (iterator.hasNext()) {
+	            String next = iterator.next();
+	            System.out.println(next);
+	        }	
+	        
 	}  
-   public static String  clientTest(String url, String json){
-	   json="";
-	   // 创建Httpclient对象
-       CloseableHttpClient httpClient = HttpClients.createDefault();
-       CloseableHttpResponse response = null;
-       String resultString = "";
-       try {
-           // 创建Http Post请求
-           HttpPost httpPost = new HttpPost(url);
-           // 创建请求内容
-           StringEntity entity = new StringEntity(json, ContentType.APPLICATION_JSON);
-           System.out.println("参数："+json);
-           httpPost.setEntity(entity);
-           // 执行http请求
-           response = httpClient.execute(httpPost);
-           resultString = EntityUtils.toString(response.getEntity(), "utf-8");
-           System.out.println("输出："+resultString);
-       } catch (Exception e) {
-           e.printStackTrace();
-           System.out.println(e.getMessage());
-       } finally {
-           try {
-               response.close();
-           } catch (IOException e) {
-               // TODO Auto-generated catch block
-               e.printStackTrace();
-               System.out.println(e.getMessage());
-           }
-       }
-
-       return resultString;
-   }
+   
 }

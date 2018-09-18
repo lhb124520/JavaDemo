291f313d8d298474561f757480a952677e715b5e
liu.hb <1282072571@qq.com>
2018-09-18 11:33:04 +0800

    修改注释

--- src/com/demo/test/JedisDemo.java
+++ src/com/demo/test/JedisDemo.java
@@ -9,7 +9,6 @@ import redis.clients.jedis.Jedis;
 public class JedisDemo {
 	public static void main(String[] args) {
 		// 连接本地的 Redis 服务
-		@SuppressWarnings("resource")
 		Jedis jedis = new Jedis("127.0.0.1");
 		System.out.println("连接成功");
 		// 查看服务是否运行
